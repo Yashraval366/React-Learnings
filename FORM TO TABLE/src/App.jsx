8211a00e8976ddb9d11a5112550ae91d1c3345c3
Yash raval <123889939+Yashraval366@users.noreply.github.com>
2024-09-22 14:45:36 +0530

    commit

--- FORM TO TABLE/src/App.jsx
+++ FORM TO TABLE/src/App.jsx
@@ -4,7 +4,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { Button, Container, Row, Col } from 'react-bootstrap';
 import { createBrowserRouter, RouterProvider, Link,} from 'react-router-dom';
 import { DataProvider, useData } from './DataContext';
-import './App.css';
 import Details from './Details';
 
 
@@ -70,54 +69,140 @@ function MainApp() {
 
   return (
     <>
-      <div className='container-fluid bg-light p-5 m-0 vh-100'>
-        <div className="shadow-lg p-3 mt-1 mb-3 bg-white rounded mx-auto" style={{ width: '35%', height: '540px' }}>
+      <div className="container-fluid bg-light p-3 m-0 vh-100 d-flex align-items-center justify-content-center">
+        <div
+          className="shadow-lg p-3 bg-white rounded mx-auto"
+          style={{ width: '90%', maxWidth: '500px', height: 'auto' }}
+        >
           <div className="mb-2 mt-1">
-            <label htmlFor="Name" className="mb-1 fs-6">Name: </label>
-            <input type="text" className="form-control" id="Name" placeholder="Name" name="Name" value={FormValues.Name} onChange={handlechange}/>
+            <label htmlFor="Name" className="mb-1 fs-6">
+              Name:
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="Name"
+              placeholder="Name"
+              name="Name"
+              value={FormValues.Name}
+              onChange={handlechange}
+            />
           </div>
           <div className="mb-2 mt-1">
-            <label htmlFor="Email" className="mb-1 fs-6">Email: </label>
-            <input type="Email" className="form-control" id="Email" placeholder="Email" name="Email" value={FormValues.Email} onChange={handlechange} />
+            <label htmlFor="Email" className="mb-1 fs-6">
+              Email:
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="Email"
+              placeholder="Email"
+              name="Email"
+              value={FormValues.Email}
+              onChange={handlechange}
+            />
           </div>
           <div className="mb-2 mt-1">
-            <label htmlFor="occupation" className="form-label" >Select occupation</label>
-            <select className="form-select" id="occupation" name='occupation' value={FormValues.occupation} onChange={handlechange}>
-              <option value="" disabled>Select an option</option> 
-              <option name="Student" value="Student">Student</option>
-              <option name="Employee" value="Employee">Employee</option>
-              <option name="other" value= "other" >other</option>
+            <label htmlFor="occupation" className="form-label">
+              Select occupation
+            </label>
+            <select
+              className="form-select"
+              id="occupation"
+              name="occupation"
+              value={FormValues.occupation}
+              onChange={handlechange}
+            >
+              <option value="" disabled>
+                Select an option
+              </option>
+              <option name="Student" value="Student">
+                Student
+              </option>
+              <option name="Employee" value="Employee">
+                Employee
+              </option>
+              <option name="other" value="other">
+                Other
+              </option>
             </select>
           </div>
-          <div className='form-group mt-3'>
-            <h3 className='fs-6 '>Select gender</h3>
-            <div className='mt-0 mb-0'>
-              <input type='radio' name='gender' value='male' checked={FormValues.gender === 'male'} onChange={handlechange}/>
-              <label htmlFor='male'>Male</label>
+          <div className="form-group mt-3">
+            <h3 className="fs-6">Select gender</h3>
+            <div className="mt-0 mb-0">
+              <input
+                type="radio"
+                name="gender"
+                value="male"
+                checked={FormValues.gender === 'male'}
+                onChange={handlechange}
+              />
+              <label htmlFor="male"> Male </label>
             </div>
-            <div className='mb-3 mt-0'>
-              <input type='radio' name='gender' value='female' checked={FormValues.gender === 'female'} onChange={handlechange}/>
-              <label htmlFor='female'>Female</label>
+            <div className="mb-3 mt-0">
+              <input
+                type="radio"
+                name="gender"
+                value="female"
+                checked={FormValues.gender === 'female'}
+                onChange={handlechange}
+              />
+              <label htmlFor="female"> Female </label>
             </div>
           </div>
-          <div className='form-group mt-1'>
-            <h3 className='fs-6'>Select Language</h3>
-            <label className='form-label mb-0'><input type='checkbox' name="languages" value="html" checked={FormValues.languages.includes('html')} onChange={handlechange} />html</label><br/>
-            <label className='form-label mb-0'><input type='checkbox' name="languages" value="css" checked={FormValues.languages.includes('css')} onChange={handlechange}/>CSS</label><br/>
-            <label className='form-label mb-2'><input type='checkbox' name="languages" value="Javascript" checked={FormValues.languages.includes('Javascript')} onChange={handlechange}/>Javascript</label>
+          <div className="form-group mt-1">
+            <h3 className="fs-6">Select Language</h3>
+            <label className="form-label mb-0">
+              <input
+                type="checkbox"
+                name="languages"
+                value="html"
+                checked={FormValues.languages.includes('html')}
+                onChange={handlechange}
+              />
+              HTML
+            </label>
+            <br />
+            <label className="form-label mb-0">
+              <input
+                type="checkbox"
+                name="languages"
+                value="css"
+                checked={FormValues.languages.includes('css')}
+                onChange={handlechange}
+              />
+              CSS
+            </label>
+            <br />
+            <label className="form-label mb-2">
+              <input
+                type="checkbox"
+                name="languages"
+                value="Javascript"
+                checked={FormValues.languages.includes('Javascript')}
+                onChange={handlechange}
+              />
+              Javascript
+            </label>
           </div>
           <div>
             <button
               type="button"
-              className="btn btn-primary mt-3"
+              className="btn btn-primary mt-3 w-100"
               onClick={handlesubmit}
             >
               Submit
             </button>
-            {showbtn && (<Link to="/details"><button className='btn btn-outline-primary mt-3 ms-3'>check details</button></Link>)}
+            {showbtn && (
+              <Link to="/details">
+                <button className="btn btn-outline-primary mt-3 w-100">
+                  Check Details
+                </button>
+              </Link>
+            )}
           </div>
         </div>
-      </div>       
+      </div>
     </>
   )
 }
--- FORM TO TABLE/src/Details.jsx
+++ FORM TO TABLE/src/Details.jsx
@@ -9,31 +9,36 @@ function Details() {
 
   return (
     <>
-      <div className='shadow-lg p-3 mt-4 mb-3 bg-white rounded mx-auto' style={{ width: '40%', height: '300px' }}>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Email</th>
-              <th scope="col">occupation</th>
-              <th scope="col">gender</th>
-              <th scope="col">languages</th>
-            </tr>
-          </thead>
-          <tbody>
-            {submittedData.map((data, index) => (
-              <tr key={index}>
-                <td>{index +1}</td>
-                <td>{data.Name}</td>
-                <td>{data.Email}</td>
-                <td>{data.occupation || 'N/A'}</td>
-                <td>{data.gender}</td>
-                <td>{data.languages.join(', ')}</td>
+      <div
+        className="container-fluid d-flex shadow-lg p-3 mt-4 mb-3 bg-white rounded align-items-center justify-content-center"
+        style={{ width: '90%', maxWidth: '570px', height: 'auto' }}
+      >
+        <div className="table-responsive w-100">
+          <table className="table table-bordered table-hover mx-auto">
+            <thead className="table-light">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Occupation</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Languages</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {submittedData.map((data, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{data.Name}</td>
+                  <td>{data.Email}</td>
+                  <td>{data.occupation || 'N/A'}</td>
+                  <td>{data.gender}</td>
+                  <td>{data.languages.join(', ')}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   )
